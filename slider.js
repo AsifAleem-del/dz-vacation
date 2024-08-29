1985cc1e0481ab4944eadc1a986a03b54623ea2a
AsifAleem-del <asifaleemkhan@gmail.com>
2024-08-29 19:38:57 +0530

    Update files.

--- slider.js
+++ slider.js
@@ -71,3 +71,23 @@ function showSlider(type){
       }
     }
     window.addEventListener('scroll', reveal);
+
+
+
+
+// scroll top coding start from here
+let myButton = document.getElementById("myBtn");
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+function scrollFunction(){
+  if (document.bodyscrollTop > 20 || document.documentElement.scrollTop > 20 ){
+    myButton.style.display =  "block";
+  } else {
+    myButton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction(){
+document.body.scrollTop = 0;
+document.documentElement.scrollTop = 0;
+}
